77708c1c085f9bd9209d052e1c9bbc57a43c93d5
SaulCamacho <saulcamacho319@aragon.unam.mx>
2022-03-02 23:00:10 -0600

    Sube java

---
+++ sesiones/Conceptos_Basicos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conceptos.basicos;
+
+/**
+ *
+ * @author Dell
+ */
+public class ConceptosBasicos {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Primer java 02/03/2022");// TODO code application logic here
+    }
+}
